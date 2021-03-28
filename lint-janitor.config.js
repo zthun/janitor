6c084aa8eb15049d979734a122e1adaa8026082b
Anthony Bonta <zthun.coding@gmail.com>
2021-03-28 15:40:44 -0500

    chore: readd support for html and markdown files

--- lint-janitor.config.js
+++ lint-janitor.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  esFiles: ['packages/**/src/**/*.ts', 'packages/**/src/**/*.js'],
+  esFiles: ['packages/**/src/**/*.ts', 'packages/*-config/index.js'],
   styleFiles: ['test/**/*.less', 'test/**/*.scss'],
-  // htmlFiles: ['test/**/*.html'],
-  // markdownFiles: ['./*.md'],
+  htmlFiles: ['test/**/*.html'],
+  markdownFiles: ['./*.md'],
   jsonFiles: ['*.json', '.eslintrc', '.htmlhintrc', 'lint/.htmlhintrc', 'packages/**/*.json'],
   yamlFiles: ['./.travis.yml']
 };
