e0444019a1346397ddaae086ccf1f846fbeaa2b3
Anthony Bonta <zthun.coding@gmail.com>
2021-03-27 23:47:23 -0500

    chore: added missing files

---
+++ packages/stylelint-config/src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'max-nesting-depth': 4,
+    'no-descending-specificity': null,
+    'declaration-empty-line-before': 'never'
+  }
+};
