0122169c6a7f4daffbc9eaca1f89df2d89323228
Anthony Bonta <zthun.coding@gmail.com>
2021-04-02 21:34:10 -0500

    chore: better lint targets

--- lint-janitor.config.js
+++ lint-janitor.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  esFiles: ['packages/**/src/**/*.ts', 'packages/*-config/index.js'],
+  esFiles: ['*.js', 'packages/**/src/**/*.ts', 'packages/*-config/index.js'],
   styleFiles: ['test/**/*.less', 'test/**/*.scss'],
   htmlFiles: ['test/**/*.html'],
-  markdownFiles: ['./*.md', 'packages/**/*.md'],
+  markdownFiles: ['*.md', 'packages/**/*.md'],
   jsonFiles: ['*.json', 'packages/**/*.json'],
-  yamlFiles: ['./.travis.yml']
+  yamlFiles: ['*.yml']
 };
