d92eac7ed23263e06a3c1886071268d107a3d05e
Anthony Bonta <zthun.coding@gmail.com>
2021-05-08 14:51:30 -0500

    build: fix the lint errors from changelog generation

--- lint-janitor.config.js
+++ lint-janitor.config.js
@@ -1,10 +1,15 @@
+var generated = ['**/CHANGELOG.md'];
+
 module.exports = {
   esFiles: ['*.js', 'packages/**/src/**/*.ts', 'packages/*-config/index.js'],
   styleFiles: ['test/**/*.less', 'test/**/*.scss'],
   htmlFiles: ['test/**/*.html'],
   markdownFiles: ['*.md', 'packages/**/*.md'],
+  markdownFilesExclude: generated,
   jsonFiles: ['*.json', 'packages/**/*.json'],
   yamlFiles: ['*.yml'],
   spellingFiles: ['packages/**/src/**/*.ts', '*.md', 'packages/**/*.md', 'packages/**/*.json'],
-  prettyFiles: ['packages/**/src/**/*.ts', '*.md', 'packages/**/*.md', 'packages/**/*.json']
+  spellingFilesExclude: generated,
+  prettyFiles: ['packages/**/src/**/*.ts', '*.md', 'packages/**/*.json'],
+  prettyFilesExclude: generated
 };
