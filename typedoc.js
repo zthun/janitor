d9bc7ccade532025e14bb02c98adab75b8809851
Anthony Bonta <zthun.coding@gmail.com>
2021-07-24 14:49:51 -0500

    fix: typedoc documentation should now properly generate

--- typedoc.js
+++ typedoc.js
@@ -1,7 +1,6 @@
 module.exports = {
   out: 'docs/typedoc',
   json: 'docs/typedoc.json',
-  exclude: ['src/index.ts'],
   excludePrivate: true,
   tsconfig: 'tsconfig.bin.json',
   entryPoints: ['src/index.ts']
