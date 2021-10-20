35b20f08618034c8ee39b9ed97dcb5058245a1e0
Anthony Bonta <zthun.coding@gmail.com>
2021-10-19 22:54:45 -0500

    refactor: remove redundant options

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,18 +1,8 @@
 module.exports = {
-  arrowParens: 'always',
-  bracketSpacing: true,
   endOfLine: 'auto',
-  htmlWhitespaceSensitivity: 'css',
   jsxSingleQuote: true,
-  jsxBracketSameLine: false,
   printWidth: 256,
-  proseWrap: 'preserve',
   quoteProps: 'consistent',
-  requirePragma: false,
-  semi: true,
   singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'none',
-  useTabs: false,
-  vueIndentScriptAndStyle: false
+  trailingComma: 'none'
 };
