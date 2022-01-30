04e788ad412e228806751e95c8a90a6d389f3209
Anthony Bonta <zthun.coding@gmail.com>
2022-01-30 05:35:50 -0600

    feat: added eslint-react

--- packages/lint-janitor-config/index.js
+++ packages/lint-janitor-config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   configs: {
     eslint: require('./eslint'),
+    eslintReact: require('./eslint-react'),
     htmlhint: require('./htmlhint'),
     markdownlint: require('./markdownlint'),
     prettier: require('./prettier'),
