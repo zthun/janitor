7da19eb57ad95eeb0fb860a2feb1f863b8d38200
Anthony Bonta <zthun.coding@gmail.com>
2023-11-29 11:18:48 -0600

    fix: added additional stylelint-configs

--- packages/lint-janitor-config/index.js
+++ packages/lint-janitor-config/index.js
@@ -1,10 +1,12 @@
 module.exports = {
   configs: {
     eslint: require('./eslint'),
-    eslintReact: require('./eslint-react-config'),
+    eslintReact: require('./eslint-react'),
     htmlhint: require('./htmlhint'),
     markdownlint: require('./markdownlint'),
     prettier: require('./prettier'),
-    stylelint: require('./stylelint')
+    stylelint: require('./stylelint'),
+    stylelintLess: require('./stylelint-less'),
+    stylelintSass: require('./stylelint-sass')
   }
 };
