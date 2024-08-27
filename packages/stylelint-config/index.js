821cff6256ee487418e6fa18e686b5c911dba9f6
Anthony Bonta <zthun.coding@gmail.com>
2024-08-27 01:53:18 -0500

    refactor!: stylelint-config now favors the default rule set

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,9 +1,3 @@
 module.exports = {
   extends: ["stylelint-config-standard"],
-  rules: {
-    "at-rule-no-unknown": null,
-    "max-nesting-depth": 4,
-    "no-descending-specificity": null,
-    "declaration-empty-line-before": "never",
-  },
 };
