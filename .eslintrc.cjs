f44a0521b57c19e49bb2ae073b9c5b9c61ac47f5
Anthony Bonta <zthun.coding@gmail.com>
2024-11-05 14:56:06 -0600

    build: update eslint to 9.x

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  ...require("@zthun/lint-janitor-config/eslint/react"),
-  env: {
-    node: true,
-  },
-};
---
+++ eslint.config.mjs
@@ -0,0 +1,2 @@
+import { react, vanilla } from "@zthun/lint-janitor-config/eslint";
+export default [...vanilla, ...react];
