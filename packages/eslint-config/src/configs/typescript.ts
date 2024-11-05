e94db899d9ea10d330dc908e0919b9c0a4d5b6bf
Anthony Bonta <zthun.coding@gmail.com>
2024-11-05 16:23:31 -0600

    feat: standards for typescript are now composable

---
+++ packages/eslint-config/src/configs/typescript.ts
@@ -0,0 +1,48 @@
+import { Linter } from "eslint";
+import ts from "typescript-eslint";
+
+export const typescript = [
+  ...ts.configs.recommended,
+  {
+    rules: {
+      // There are times when any and legacy namespaces makes sense.
+      // If you use any kind of decorator library, you will almost
+      // be required to use any at some point.  Forcing a non use of them
+      // means you're spending a bunch of time play type gymnastics and to
+      // hell with that.
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-namespace": "off",
+
+      // Would be fine, but there's a bug in this where you have a function with
+      // access arguments.  Those constructors are often empty - so we want to let
+      // a part of this one through.
+      "no-empty-function": "off",
+      "@typescript-eslint/no-empty-function": [
+        "error",
+        { allow: ["constructors"] },
+      ],
+
+      // A lot of 3rd party libraries still don't support esm
+      // and trying to force this right now just isn't feasible.
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-require-imports": "off",
+
+      // I want aliasing support.
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+
+      // You will need unsafe declaration merging if you are doing anything
+      // with decorators as what often happens is that TypeScript cannot infer
+      // the output type of a decorator. So this has to be on to deal with
+      // TypeScripts shortcoming in this department.  See
+      // https://github.com/microsoft/TypeScript/issues/4881 for more information.
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+
+      // I can technically agree with this, but where this comes in
+      // handy is unit testing and I value that, so I want support
+      // to make the assumption that I know what I'm doing when
+      // I make a non-null assertion.
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  } satisfies Linter.Config,
+];
