94d282248a7bc2bfb9d785ac028372fd95abccf5
Anthony Bonta <zthun.coding@gmail.com>
2024-11-05 00:51:23 -0600

    docs: prettier config reasoning

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,6 +1,9 @@
 import { Config } from "prettier";
 
 const config: Config = {
+  // Markdown will not format correct when ran through
+  // prettier, so proseWrap is required always to enable
+  // support for it.
   proseWrap: "always",
 };
 
