2d0abd4b8f4fd1871502d084081ef6772a805ab1
Anthony Bonta <zthun.coding@gmail.com>
2024-11-05 16:23:20 -0600

    feat: standards for javascript are now composable

---
+++ packages/eslint-config/src/configs/javascript.ts
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import { Linter } from "eslint";
+
+export const javascript: Linter.Config[] = [
+  js.configs.recommended,
+  {
+    rules: {
+      // We want to support == null so we get a good check for undefined
+      // or null
+      eqeqeq: ["error", "smart"],
+      // Would be fine, but there's a bug in this where you have a function with
+      // access arguments.  Those constructors are often empty - so we want to let
+      // a part of this one through.
+      "no-empty-function": "off",
+    },
+  },
+];
