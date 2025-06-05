e8e4578711efccef9e23ade8450f07db2178127f
Anthony Bonta <zthun.coding@gmail.com>
2025-06-04 22:53:29 -0500

    refactor!: janitor lint options is now janitor options

--- packages/janitor-lint/src/index.ts
+++ packages/janitor-lint/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./app/janitor-lint-args.mjs";
-export * from "./app/janitor-lint-options.mjs";
 export * from "./app/janitor-lint.mjs";
+export * from "./app/janitor-options.mjs";
 export * from "./config/config-discovery.mjs";
 export * from "./config/config-extender.mjs";
 export * from "./config/config-reader-cosmic.mjs";
