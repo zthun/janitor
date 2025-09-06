f12d84ac1943848cdc9de2960c7de0a5834fb6da
Anthony Bonta <zthun.coding@gmail.com>
2025-09-06 13:57:36 -0500

    build: simplify external plugin

--- packages/janitor-build-config/src/plugin/vite-plugin-externalize-deps.ts
+++ packages/janitor-build-config/src/plugin/vite-plugin-externalize-deps.ts
@@ -1,133 +1,60 @@
-import { existsSync, readFileSync } from "node:fs";
+import { existsSync, readFileSync, type PathLike } from "node:fs";
 import { builtinModules } from "node:module";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import type { Plugin } from "vite";
 
 // Original source code from
 // https://github.com/davidmyersdev/vite-plugin-externalize-deps/blob/main/src/index.ts
 // It takes to long to get this plugin updated, so I just copied the source code into
-// here.  I don't think this plugin is maintained much anymore.
+// here.  I don't think this plugin is maintained much anymore.  I've greatly reduced the
+// options from this to be just a basic check for stuff in your package.json and node
+// builtins.
 
-interface UserOptions {
-  deps: boolean;
-  devDeps: boolean;
-  except: Array<string | RegExp>;
-  include: Array<string | RegExp>;
-  nodeBuiltins: boolean;
-  optionalDeps: boolean;
-  peerDeps: boolean;
-  useFile: string;
+export interface ExternalizeOptions {
+  packageJson?: PathLike;
 }
 
-export const externalizeDeps = (options: Partial<UserOptions> = {}): Plugin => {
-  const parseFile = (file: string) => {
-    return JSON.parse(readFileSync(file).toString());
-  };
-
-  const optionsResolved: UserOptions = {
-    deps: true,
-    devDeps: false,
-    except: [],
-    include: [],
-    nodeBuiltins: true,
-    optionalDeps: true,
-    peerDeps: true,
-    useFile: join(process.cwd(), "package.json"),
-    // User options take priority.
-    ...options,
-  };
+export const externalizeDeps = (options: ExternalizeOptions = {}): Plugin => {
+  const { packageJson = resolve(process.cwd(), "package.json") } = options;
 
   return {
     name: "vite-plugin-externalize-deps",
     config: () => {
-      if (!existsSync(optionsResolved.useFile)) {
-        throw new Error(
-          `[vite-plugin-externalize-deps] The file specified for useFile (${optionsResolved.useFile}) does not exist.`,
-        );
-      }
-
       const externalDeps = new Set<RegExp>();
+
+      if (!existsSync(packageJson)) {
+        const context = "janitor-build-config:externalizeDeps";
+        const msg = `[${context}] Could not find package.json at ${packageJson}.`;
+        throw new Error(msg);
+      }
+
+      const contents = readFileSync(packageJson).toString();
       const {
-        dependencies = {},
-        devDependencies = {},
-        optionalDependencies = {},
-        peerDependencies = {},
-      } = parseFile(optionsResolved.useFile);
+        dependencies = [],
+        devDependencies = [],
+        optionalDependencies = [],
+        peerDependencies = [],
+      } = JSON.parse(contents);
 
-      if (optionsResolved.deps) {
-        Object.keys(dependencies).forEach((dep) => {
-          const depMatcher = new RegExp(`^${dep}(?:/.+)?$`);
+      const push = externalDeps.add.bind(externalDeps);
 
-          externalDeps.add(depMatcher);
-        });
-      }
+      Object.keys(dependencies)
+        .concat(Object.keys(devDependencies))
+        .concat(Object.keys(optionalDependencies))
+        .concat(Object.keys(peerDependencies))
+        .map((dep) => new RegExp(`^${dep}(?:/.+)?$`))
+        .forEach(push);
 
-      if (optionsResolved.devDeps) {
-        Object.keys(devDependencies).forEach((dep) => {
-          const depMatcher = new RegExp(`^${dep}(?:/.+)?$`);
-
-          externalDeps.add(depMatcher);
-        });
-      }
-
-      if (optionsResolved.nodeBuiltins) {
-        builtinModules.forEach((builtinModule) => {
-          const builtinMatcher = new RegExp(`^(?:node:)?${builtinModule}$`);
-
-          externalDeps.add(builtinMatcher);
-        });
-      }
-
-      if (optionsResolved.optionalDeps) {
-        Object.keys(optionalDependencies).forEach((dep) => {
-          const depMatcher = new RegExp(`^${dep}(?:/.+)?$`);
-
-          externalDeps.add(depMatcher);
-        });
-      }
-
-      if (optionsResolved.peerDeps) {
-        Object.keys(peerDependencies).forEach((dep) => {
-          const depMatcher = new RegExp(`^${dep}(?:/.+)?$`);
-
-          externalDeps.add(depMatcher);
-        });
-      }
+      builtinModules
+        .map((module) => new RegExp(`^(?:node:)?${module}$`))
+        .forEach(push);
 
       const depMatchers = Array.from(externalDeps);
-
-      const isException = (id: string) => {
-        return optionsResolved.except.some((exception) => {
-          if (typeof exception === "string") {
-            return exception === id;
-          }
-
-          return exception.test(id);
-        });
-      };
-
-      const isIncluded = (id: string) => {
-        return optionsResolved.include.some((included) => {
-          if (typeof included === "string") {
-            return included === id;
-          }
-
-          return included.test(id);
-        });
-      };
 
       return {
         build: {
           rollupOptions: {
             external: (source) => {
-              if (isException(source)) {
-                return false;
-              }
-
-              if (isIncluded(source)) {
-                return true;
-              }
-
               return depMatchers.some((depMatcher) => depMatcher.test(source));
             },
           },
