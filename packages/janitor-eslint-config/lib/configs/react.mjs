d0040b55ae036f3be934c7e8382e9492b400720f
Anthony Bonta <zthun.coding@gmail.com>
2026-08-09 13:56:40 -0500

    fix: removed react-hooks plugin
    This is  contained in @eslint-react/eslint-plugin

--- packages/janitor-eslint-config/lib/configs/react.mjs
+++ packages/janitor-eslint-config/lib/configs/react.mjs
@@ -1,6 +1,5 @@
 import _react from "@eslint-react/eslint-plugin";
 import { defineConfig } from "eslint/config";
-import _hooks from "eslint-plugin-react-hooks";
 
 import { ExtEs, files } from "../files/files.mjs";
 
@@ -16,18 +15,5 @@ export const react = defineConfig([
   {
     ..._react.configs.recommended,
     files: files(...ExtEs),
-  },
-  {
-    ..._hooks.configs.flat.recommended,
-    files: files(...ExtEs),
-  },
-  {
-    files: files(...ExtEs),
-    rules: {
-      // There are times when refs are appropriate, especially if you are using
-      // html dialog based elements, which you will most likely need to use
-      // refs to move or focus.
-      "react-hooks/refs": "off",
-    },
   },
 ]);
